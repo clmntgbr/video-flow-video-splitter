878439a14da55c8c9bbb9c67df06254edb0bd956
clmntgbr <clement.goubier@gmail.com>
2025-02-15 20:44:14 +0100

    Implement subtitle merging functionality with S3 file handling and timestamp shifting

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import re
 from kombu import Queue
 from flask import Flask
 from celery import Celery
@@ -35,4 +36,84 @@ celery.conf.update({
 @celery.task(name='tasks.process_message', queue=app.config['RMQ_QUEUE_READ'])
 def process_message(message):
     apiToSubtitleMerger: ApiToSubtitleMerger = ProtobufConverter.json_to_protobuf(message)
-    print(apiToSubtitleMerger)
+    
+    try:
+        subtitles = []
+        for subtitle in apiToSubtitleMerger.mediaPod.originalVideo.subtitles:
+            key = f"{apiToSubtitleMerger.mediaPod.userUuid}/{apiToSubtitleMerger.mediaPod.uuid}/subtitles/{subtitle}"
+            if not s3_client.download_file(key, f"/tmp/{subtitle}"):
+                raise Exception
+            subtitles.append(f"/tmp/{subtitle}")
+
+        subtitles = sorted(subtitles, key=lambda x: int(re.search(r'_(\d+)\.srt$', x).group(1)))
+
+        mergedSubtitles = []
+        currentOffset = 0
+        subtitleIndex = 1
+
+        for file in subtitles:
+            parseSubtitles = parse_srt(file)
+            for _, timestamps, text in parseSubtitles:
+                new_timestamps = shift_timestamps(timestamps, currentOffset)
+                mergedSubtitles.append(f"{subtitleIndex}\n{new_timestamps}\n{text}\n\n")
+                subtitleIndex += 1
+            
+            currentOffset += 300
+
+        srtFile = apiToSubtitleMerger.mediaPod.originalVideo.name.replace(".mp4", ".srt")
+        tmpSrtFilePath = f"/tmp/{srtFile}"
+        with open(tmpSrtFilePath, 'w', encoding='utf-8') as f:
+            f.writelines(mergedSubtitles)
+
+        key = f"{apiToSubtitleMerger.mediaPod.userUuid}/{apiToSubtitleMerger.mediaPod.uuid}/{srtFile}"
+        if not s3_client.upload_file(tmpSrtFilePath, key):
+            raise Exception
+        
+        apiToSubtitleMerger.mediaPod.originalVideo.subtitle = srtFile
+        apiToSubtitleMerger.mediaPod.status = 'subtitle_merger_complete'
+        
+        file_client.delete_file(tmpSrtFilePath)
+        for subtitle in subtitles:
+            file_client.delete_file(subtitle)
+
+        rmq_client.send_message(apiToSubtitleMerger, "App\\Protobuf\\SubtitleMergerToApi")
+        return True
+    except Exception as e:
+        apiToSubtitleMerger.mediaPod.status = 'subtitle_merger_error'
+        if not rmq_client.send_message(apiToSubtitleMerger, "App\\Protobuf\\SubtitleMergerToApi"):
+            return False
+
+def parse_srt(tmpSrtFilePath):
+    subtitles = []
+    with open(tmpSrtFilePath, 'r', encoding='utf-8') as file:
+        content = file.read().strip()
+    
+    entries = re.split(r'\n\n+', content)
+    for entry in entries:
+        lines = entry.split("\n")
+        if len(lines) >= 3:
+            num = int(lines[0])
+            timestamps = lines[1]
+            text = "\n".join(lines[2:])
+            subtitles.append((num, timestamps, text))
+    
+    return subtitles
+
+def shift_timestamps(timestamps, offset_seconds):
+    def convertToMs(timestamp):
+        match = re.match(r"(\d+):(\d+):(\d+),(\d+)", timestamp)
+        if not match:
+            raise ValueError(f"Format de timestamp invalide : {timestamp}")
+        h, m, s, ms = map(int, match.groups())
+        return (h * 3600 + m * 60 + s) * 1000 + ms
+    
+    def convertFromMs(ms):
+        h, ms = divmod(ms, 3600000)
+        m, ms = divmod(ms, 60000)
+        s, ms = divmod(ms, 1000)
+        return f"{h:02}:{m:02}:{s:02},{ms:03}"
+
+    start, end = timestamps.split(" --> ")
+    start_ms = convertToMs(start) + offset_seconds * 1000
+    end_ms = convertToMs(end) + offset_seconds * 1000
+    return f"{convertFromMs(start_ms)} --> {convertFromMs(end_ms)}"
